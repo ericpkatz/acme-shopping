4ea7fee2e93ee86f81f5b08c0a46e1c308135fae
ericpkatz <ericpkatz@gmail.com>
2022-08-10 19:31:15 -0400

    refactor tests plus

--- spec.js
+++ spec.js
@@ -26,43 +26,132 @@ describe('Shopping', ()=> {
     });
   });
   describe('The Shopping Experience', ()=> {
-    it('Allows the user to create an order', async()=> {
+    it('User can create an order', async()=> {
       const moe = await User.findOne({
         where: {
           username: 'moe'
-        }
-      });
-      const foo = await Product.findOne({
-        where: {
-          name: 'foo'
         }
       });
       const token = jwt.sign({ id: moe.id}, process.env.JWT);
 
       let response = await app.get('/api/orders/cart')
         .set('authorization', token);
+      response = await app.post('/api/orders')
+        .set('authorization', token);
       expect(response.status).to.equal(200);
+      console.log(response.body);
+
+    });
+    it('For first request, user gets an empty cart', async()=> {
+      const moe = await User.findOne({
+        where: {
+          username: 'moe'
+        }
+      });
+      const token = jwt.sign({ id: moe.id}, process.env.JWT);
+
+      const response = await app.get('/api/orders/cart')
+        .set('authorization', token);
+      expect(response.status).to.equal(200);
+      expect(response.body.lineItems).to.eql([]);
+
+    });
+    it('The same cart is returned to user', async()=> {
+      const moe = await User.findOne({
+        where: {
+          username: 'moe'
+        }
+      });
+      const token = jwt.sign({ id: moe.id}, process.env.JWT);
+
+      let response = await app.get('/api/orders/cart')
+        .set('authorization', token);
       const cart = response.body;
-      const id = cart.id;
-      expect(cart.id).to.be.ok;
-      expect(cart.lineItems).to.eql([]);
       response = await app.get('/api/orders/cart')
         .set('authorization', token);
-      expect(response.body.id).to.equal(id);
-      expect(response.body.lineItems).to.eql([]);
-      response = await app.post('/api/orders/cart')
-        .set('authorization', token)
-        .send({ product: foo, quantity: 1 });
-      expect(response.status).to.equal(200);
-      let lineItem = response.body.lineItems.find(lineItem => lineItem.productId === foo.id );
-      expect(lineItem).to.be.ok;
-      response = await app.post('/api/orders/cart')
-        .set('authorization', token)
-        .send({ product: foo, quantity: 2 });
-      expect(response.status).to.equal(200);
-      lineItem = response.body.lineItems.find(lineItem => lineItem.productId === foo.id );
-      expect(lineItem).to.be.ok;
-      expect(lineItem.quantity).to.equal(3);
+      expect(response.body.id).to.equal(cart.id);
+
+    });
+    describe('adding an item to a cart', ()=> {
+      describe('setting quantity to zero', ()=> {
+        it('remove it from cart', async()=> {
+          const moe = await User.findOne({
+            where: {
+              username: 'moe'
+            }
+          });
+          const foo = await Product.findOne({
+            where: {
+              name: 'foo'
+            }
+          });
+          const token = jwt.sign({ id: moe.id}, process.env.JWT);
+
+          let response = await app.put('/api/orders/cart')
+            .set('authorization', token)
+            .send({ product: foo, quantity: 3});
+          expect(response.status).to.equal(200);
+          const lineItem = response.body.lineItems.find( lineItem => lineItem.productId === foo.id);
+          expect(lineItem).to.be.ok;
+          response = await app.put('/api/orders/cart')
+            .set('authorization', token)
+            .send({ product: foo, quantity: 0});
+          expect(response.body.lineItems).to.eql([]);
+
+        });
+      });
+      describe('for the first time', ()=> {
+        it('creates a line item', async()=> {
+          const moe = await User.findOne({
+            where: {
+              username: 'moe'
+            }
+          });
+          const foo = await Product.findOne({
+            where: {
+              name: 'foo'
+            }
+          });
+          const token = jwt.sign({ id: moe.id}, process.env.JWT);
+
+          const response = await app.put('/api/orders/cart')
+            .set('authorization', token)
+            .send({ product: foo, quantity: 3});
+          expect(response.status).to.equal(200);
+          const lineItem = response.body.lineItems.find( lineItem => lineItem.productId === foo.id);
+          expect(lineItem).to.be.ok;
+
+        });
+      });
+      describe('not the first time', ()=> {
+        it('updates a lineItem', async()=> {
+          const moe = await User.findOne({
+            where: {
+              username: 'moe'
+            }
+          });
+          const foo = await Product.findOne({
+            where: {
+              name: 'foo'
+            }
+          });
+          const token = jwt.sign({ id: moe.id}, process.env.JWT);
+
+          let response = await app.put('/api/orders/cart')
+            .set('authorization', token)
+            .send({ product: foo, quantity: 3});
+
+          expect(response.status).to.equal(200);
+          const lineItem = response.body.lineItems.find( lineItem => lineItem.productId === foo.id);
+          response = await app.put('/api/orders/cart')
+            .set('authorization', token)
+            .send({ product: foo, quantity: 7});
+          const updatedLineItem = response.body.lineItems.find( lineItem => lineItem.productId === foo.id);
+          expect(updatedLineItem.id).to.equal(lineItem.id);
+
+
+        });
+      });
     });
   });
 });
