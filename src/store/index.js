88980604061d82f19b407e26b1912ee579df8e94
divineheart <100250310+Diane-Sayos@users.noreply.github.com>
2022-08-13 13:59:27 -0400

    added product on store//reorganize some files

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import auth from './auth';
 import cart from './cart';
+import products from './products';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 const reducer = combineReducers({
   auth,
-  cart
+  cart,
+  products
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
@@ -15,3 +17,4 @@ const store = createStore(reducer, applyMiddleware(thunk, logger));
 export default store;
 export * from './auth';
 export * from './cart';
+export * from './products';
