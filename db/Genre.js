8ee776fd5990c23aa1b198e75dc97b0d1d278391
Orzu Tursunova <orzujke@gmail.com>
2022-08-13 12:21:16 -0500

    changed model names

--- db/Genre.js
+++ db/Genre.js
@@ -1,11 +1,6 @@
 const conn = require("./conn");
 const { Sequelize } = conn;
 
-const Genre = conn.define("order", {
-  isCart: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: true,
-  },
-});
+const Genre = conn.define("genre", {});
 
 module.exports = Genre;
