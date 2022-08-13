7e650a8e1b4e185609050ab67eddc67b5291b068
Orzu Tursunova <orzujke@gmail.com>
2022-08-13 12:25:56 -0500

    added new columns to User model

--- db/User.js
+++ db/User.js
@@ -1,106 +1,164 @@
-const conn = require('./conn');
+const conn = require("./conn");
 const { Sequelize } = conn;
+const { STRING, INTEGER, ENUM, DATEONLY } = Sequelize;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-
-
-const User = conn.define('user', {
+const User = conn.define("user", {
+  firstName: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  lastName: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  phone: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  shippingAddressApt: {
+    type: STRING,
+  },
+  shippingAddresStr: {
+    type: STRING,
+    allowNull: false,
+  },
+  shippingAddresCity: {
+    type: STRING,
+    allowNull: false,
+  },
+  shippingAddresState: {
+    type: STRING,
+    allowNull: false,
+  },
+  shippingAddresZipcode: {
+    type: STRING,
+    allowNull: false,
+  },
+  creditCardNumber: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  creditCardExpirationDate: {
+    type: DATEONLY,
+    allowNull: false,
+  },
+  creditCardPin: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  userType: {
+    type: ENUM,
+    values: ["user", "admin"],
+  },
   username: {
-    type: Sequelize.STRING
+    type: STRING,
   },
   password: {
-    type: Sequelize.STRING
-  }
+    type: STRING,
+  },
 });
 
-
-User.addHook('beforeSave', async(user)=> {
+User.addHook("beforeSave", async (user) => {
   user.password = await bcrypt.hash(user.password, 5);
 });
 
-User.prototype.createOrderFromCart = async function(){
+User.prototype.createOrderFromCart = async function () {
   const cart = await this.getCart();
   cart.isCart = false;
   return cart.save();
-}
+};
 
-User.prototype.addToCart = async function({ product, quantity}){
+User.prototype.addToCart = async function ({ product, quantity }) {
   const cart = await this.getCart();
   let lineItem = await conn.models.lineItem.findOne({
     where: {
       productId: product.id,
-      orderId: cart.id
-    }
+      orderId: cart.id,
+    },
   });
-  if(lineItem){
+  if (lineItem) {
     lineItem.quantity = quantity;
-    if(lineItem.quantity){
+    if (lineItem.quantity) {
       await lineItem.save();
-    }
-    else {
+    } else {
       await lineItem.destroy();
     }
-  }
-  else {
-    await conn.models.lineItem.create({ productId: product.id, quantity, orderId: cart.id });
+  } else {
+    await conn.models.lineItem.create({
+      productId: product.id,
+      quantity,
+      orderId: cart.id,
+    });
   }
   return this.getCart();
-}
+};
 
-User.prototype.getCart = async function(){
+User.prototype.getCart = async function () {
   let order = await conn.models.order.findOne({
     where: {
       userId: this.id,
-      isCart: true
+      isCart: true,
     },
     include: [
       {
         model: conn.models.lineItem,
-        include: [ conn.models.product ]
-      }
-    ]
+        include: [conn.models.product],
+      },
+    ],
   });
-  if(!order){
+  if (!order) {
     order = await conn.models.order.create({ userId: this.id });
     order = await conn.models.order.findByPk(order.id, {
-      include: [ conn.models.lineItem ]
+      include: [conn.models.lineItem],
     });
   }
   return order;
-}
+};
 
-User.authenticate = async function(credentials){
+User.authenticate = async function (credentials) {
   const user = await this.findOne({
     where: {
-      username: credentials.username
-    } 
+      username: credentials.username,
+    },
   });
-  if(user && await bcrypt.compare(credentials.password, user.password)){
-    return jwt.sign({ id: user.id}, process.env.JWT);
-  }
-  else {
-    const error = new Error('Bad Credentials');
+  if (user && (await bcrypt.compare(credentials.password, user.password))) {
+    return jwt.sign({ id: user.id }, process.env.JWT);
+  } else {
+    const error = new Error("Bad Credentials");
     error.status = 401;
     throw error;
   }
-}
+};
 
-User.findByToken = async function findByToken(token){
+User.findByToken = async function findByToken(token) {
   try {
     const id = jwt.verify(token, process.env.JWT).id;
     const user = await User.findByPk(id);
-    if(!user){
-      throw 'error';
+    if (!user) {
+      throw "error";
     }
     return user;
-  }
-  catch(ex){
-    const error = new Error('bad token');
+  } catch (ex) {
+    const error = new Error("bad token");
     error.status = 401;
     throw error;
   }
-}
+};
 
 module.exports = User;
-
