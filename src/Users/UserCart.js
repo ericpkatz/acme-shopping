ab76e35a8d2d638c3bc19ca9474dff1ac5f92730
Alejandro Loja <alejandro.loja@gmail.com>
2022-08-22 23:18:33 -0400

    fixed decimal but and items now are sorted in cart view

--- src/Users/UserCart.js
+++ src/Users/UserCart.js
@@ -7,9 +7,9 @@
 
 import React from "react";
 import { connect } from "react-redux";
-import { updateCart } from "../store";
+import { updateLineItem, updateCart } from "../store";
 
-const UserCart = ({ cart, subtotal, increment, deleteLineItem }) => {
+const UserCart = ({ lineItems, subtotal, increment, deleteLineItem }) => {
   return (
     <section>
       <table className="table">
@@ -22,20 +22,35 @@ const UserCart = ({ cart, subtotal, increment, deleteLineItem }) => {
             <th>Total</th>
             <th></th>
           </tr>
-          {(cart.lineItems || []).map((lineItem) => {
+          {(lineItems || []).map((lineItem) => {
             let subtotal = lineItem.product.price * lineItem.quantity;
             return (
               <tr key={lineItem.id}>
-                  <td><img src={lineItem.product.imgUrl} width='120' height='80'/></td>
-                  <td>{lineItem.product.name}</td>
-                  <td>
-                    <button onClick={() => increment(lineItem, -1)} disabled={lineItem.quantity === 1}>-</button>
-                    {lineItem.quantity}
-                    <button onClick={() => increment(lineItem, +1)}>+</button>
-                  </td>
-                  <td>${lineItem.product.price}</td>
-                  <td>${ subtotal }</td>
-                  <td><button onClick={() => {deleteLineItem(lineItem)}}>Remove</button></td>
+                <td>
+                  <img src={lineItem.product.imgUrl} width="120" height="80" />
+                </td>
+                <td>{lineItem.product.name}</td>
+                <td>
+                  <button
+                    onClick={() => increment(lineItem, -1)}
+                    disabled={lineItem.quantity === 1}
+                  >
+                    -
+                  </button>
+                  {lineItem.quantity}
+                  <button onClick={() => increment(lineItem, +1)}>+</button>
+                </td>
+                <td>${lineItem.product.price}</td>
+                <td>${subtotal && subtotal.toFixed(2)}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      deleteLineItem(lineItem);
+                    }}
+                  >
+                    Remove
+                  </button>
+                </td>
               </tr>
             );
           })}
@@ -45,7 +60,9 @@ const UserCart = ({ cart, subtotal, increment, deleteLineItem }) => {
             <th></th>
             <th></th>
             <th>Subtotal: ${subtotal}</th>
-            <th><button>Continue to Checkout</button></th>
+            <th>
+              <button>Continue to Checkout</button>
+            </th>
           </tr>
         </tbody>
       </table>
@@ -63,8 +80,12 @@ const mapState = (state) => {
     }, 0);
   };
 
+  //Sorts the Line Items
+  (state.cart.lineItems || []).sort(function (a, b) {
+    return a.id - b.id;
+  });
   return {
-    cart: state.cart || [],
+    lineItems: state.cart.lineItems || [],
     subtotal: calculateSum().toFixed(2),
   };
 };
@@ -72,11 +93,11 @@ const mapDispatch = (dispatch) => {
   return {
     increment: (lineItem, dir) => {
       const item = { ...lineItem, quantity: lineItem.quantity + dir };
-      dispatch(updateCart(item));
+      dispatch(updateLineItem(item));
     },
     deleteLineItem: (lineItem) => {
       const item = { ...lineItem, quantity: 0 };
-      dispatch(updateCart(item));
+      dispatch(updateLineItem(item));
     },
   };
 };
