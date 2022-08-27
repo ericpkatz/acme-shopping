0117b427e6c1ffd62cd94a071fcc39a149cc851d
Alejandro Loja <alejandro.loja@gmail.com>
2022-08-27 00:28:08 -0400

    working on css button

--- src/Products/ProductSpecificView.js
+++ src/Products/ProductSpecificView.js
@@ -1,54 +1,86 @@
 import React from "react";
 import { connect } from "react-redux";
-import { updateCart } from '../store/cart';
+import { updateCart } from "../store/cart";
 import AddCartHelper from "./AddCartHelper";
 
-const ProductSpecificView = ({ product , addToCart, lineItems, auth }) => {
-    const openForm = () => {
-        document.getElementById("main-app").style.marginRight = '350px';
-        document.getElementById("product-form").style.width = '300px';
-    }
-    return (
-        <section className="main">
-            <div className="product">
-                <div className="image">
-                    <img src={ product.imgUrl } width='450' height='320'/>
-                </div>
-                {
-                    auth.isAdmin ?
-                    <div className="info">
-                        { product.name }<br />
-                        { product.description }<br />
-                        ML: { product.ml }<br />
-                        ${ product.price }<br />
-                        <button onClick={() => openForm()} className='open-form-btn'>Edit Product</button><br />
-                        <AddCartHelper addToCart={ addToCart } product={ product } lineItems={ lineItems } />
-                    </div> :
-                    <div className="info">
-                        { product.name }<br />
-                        { product.description }<br />
-                        ML: { product.ml }<br />
-                        ${ product.price }<br />
-                        <AddCartHelper addToCart={ addToCart } product={ product } lineItems={ lineItems } />
-                </div>
-                }
+const ProductSpecificView = ({
+  style,
+  product,
+  addToCart,
+  lineItems,
+  auth,
+}) => {
+  const openForm = () => {
+    document.getElementById("main-app").style.marginRight = "350px";
+    document.getElementById("product-form").style.width = "300px";
+  };
+  console.log(style);
+  return (
+    <section className="main">
+      <div className="product">
+        <div className="image">
+          <img src={product.imgUrl} width="450" height="320" />
+        </div>
+        {auth.isAdmin ? (
+          <div className="info">
+            {product.name}
+            <br />
+            {product.description}
+            <br />
+            ML: {product.ml}
+            <br />${product.price}
+            <br />
+            <div className={"product-add-edit-Cart-div"}>
+              <button onClick={() => openForm()} className="open-form-btn">
+                Edit Product
+              </button>
+              <br />
+              <AddCartHelper
+                addToCart={addToCart}
+                product={product}
+                lineItems={lineItems}
+                style={style}
+              />
             </div>
-        </section>
-    )
-}
+          </div>
+        ) : (
+          <div className="info">
+            {product.name}
+            <br />
+            {product.description}
+            <br />
+            ML: {product.ml}
+            <br />${product.price}
+            <br />
+            <AddCartHelper
+              b={"open-form-btn"}
+              addToCart={addToCart}
+              product={product}
+              lineItems={lineItems}
+              style={style}
+            />
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
 const mapState = (state, { match }) => {
-    return {
-        product: state.products.find(product => product.id === 1 * match.params.id) || {},
-        lineItems: state.cart.lineItems,
-         auth: state.auth
-    } 
-}
-const mapDispatch = dispatch => {
-    return {
-        addToCart: (product, quantity) => {
-            dispatch(updateCart(product, quantity));
-        }
-    }
+  return {
+    product:
+      state.products.find((product) => product.id === 1 * match.params.id) ||
+      {},
+    lineItems: state.cart.lineItems,
+    auth: state.auth,
+    style: "open-form-btn",
+  };
+};
+const mapDispatch = (dispatch) => {
+  return {
+    addToCart: (product, quantity) => {
+      dispatch(updateCart(product, quantity));
+    },
+  };
 };
 
 export default connect(mapState, mapDispatch)(ProductSpecificView);
